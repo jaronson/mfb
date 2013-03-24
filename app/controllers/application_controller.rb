df5b50e61e3698892dbab5cbecf4aebcf3bc6602
Josh Aronson <jparonson@gmail.com>
2013-03-24 12:04:03 -0500

    Add fonts js

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  include Refinery::Blog::ControllerHelper
+
+  helper :'refinery/blog/posts'
+
   protect_from_forgery
 
   before_filter :load_all_blog_categories
